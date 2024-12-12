3d934bdd8ed434b73b6031243452c4089ff259d2
jhosecar <marinjhoan2020@gmail.com>
2024-12-12 15:45:17 -0500

    Cambio en styles and html

--- static/js/titulo.js
+++ static/js/titulo.js
@@ -3,7 +3,7 @@ script.src = 'https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js';
 
 script.onload = function() {
   var typed = new Typed(".auto-type", {
-    strings: ["Energía Renovable", "100% Limpia", "La Energía del Futuro"],
+    strings: ["Energías Renovables", "La Energía del Futuro"],
     cursorChar: '|',
     startDelay: 500,
     typeSpeed: 50,
